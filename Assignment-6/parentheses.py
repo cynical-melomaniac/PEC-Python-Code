b62a01296a8df812e5a90d5a412946aff03462c6
Cynical <cynical.melomaniac@gmail.com>
2023-01-26 21:08:16 +0530

    modified:   Assignment-6/parentheses.py

--- Assignment-6/parentheses.py
+++ Assignment-6/parentheses.py
@@ -1,38 +1,20 @@
 input_string = input("Enter a string with parentheses: ")
 
 class validity_of_parentheses:
-    
-    def __init__(self, _input_string):
 
-        round_open = 0
-        square_open = 0
-        curly_open = 0
+    def __init__(self, _input_string = str()):
+        
+        parentheses_list = ['(', '{', '[', ']', '}', ')']
+        parentheses_set_list = ['()', '{}', '[]']
+        
+        for char in _input_string:
+            if char not in parentheses_list:
+                _input_string = _input_string.replace(char, '')
 
-        for parenthesis in _input_string:
-            if parenthesis == '(':
-                round_open += 1
-            
-            elif parenthesis == '[':
-                square_open += 1
-            
-            elif parenthesis == '{':
-                curly_open += 1
+        while any(parenthesis_set in _input_string for parenthesis_set in parentheses_set_list):
+            for parenthesis_set in parentheses_set_list:
+                _input_string = _input_string.replace(parenthesis_set, "", -1)
 
-
-            elif parenthesis == ')':
-                round_open -= 1
-            
-            elif parenthesis == ']':
-                square_open -= 1
-            
-            elif parenthesis == '}':
-                curly_open -= 1
-
-
-        if round_open == 0 and square_open == 0 and curly_open == 0:
-            print("Given string has valid parentheses.")
-            
-        if round_open != 0 or square_open != 0 or curly_open != 0:
-            print("Given string has invalid parentheses.")
+        print("Given parentheses are valid." if _input_string == "" else "Given parentheses are invalid.")
 
 validity_of_parentheses(input_string)
