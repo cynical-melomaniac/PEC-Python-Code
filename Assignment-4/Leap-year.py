bb61b909b451044bdfa68519f7c14a250d40c028
Cynical <cynical.melomaniac@gmail.com>
2023-01-28 11:12:03 +0530

    modified:   Assignment-4/Grading.py 	modified:   Assignment-4/Leap-year.py

--- Assignment-4/Leap-year.py
+++ Assignment-4/Leap-year.py
@@ -3,7 +3,7 @@ year = int(input("Enter Year: "))
 if (year % 4 == 0) and (year % 100 != 0):
     print("The year is a leap year.")
 
-if (year % 100 == 0) and (year % 400 == 0):
+elif (year % 100 == 0) and (year % 400 == 0):
     print("The year is a leap year.")
 
 else:
